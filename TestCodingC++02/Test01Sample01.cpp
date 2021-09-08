fa8a7ef3280c73761301d6a0394e10783ee4f917
egoquat <egoquat@users.noreply.github.com>
2021-09-08 21:46:08 +0900

    test A* gold 01

--- TestCodingC++02/Test01Sample01.cpp
+++ TestCodingC++02/Test01Sample01.cpp
@@ -12,80 +12,141 @@
 
 using namespace std;
 
-struct City {
-	string Name;
-	int Idx = -1;
-	int Pos[2];
-	City* Adj[2]{ nullptr };
-	City* GetNearst() {
-		City* adjX = Adj[0]; City* adjY = Adj[1];
-		if (adjX == nullptr) return adjY;
-		if (adjY != nullptr && GetDist(adjX, 0) > GetDist(adjY, 1)) return adjY;
-		return adjX;
+static const int IDXNONE = -1;
+template<typename T> static bool Remove(vector<T>& v, const T t)
+{
+	auto it = std::find(v.begin(), v.end(), t);
+	if (it != v.end()) {
+		std::swap(*it, v.back());
+		v.pop_back();
+		return true;
 	}
-	int GetDist(City* o, int dim) { return std::abs(Pos[dim] - o->Pos[dim]); }
-	void SetAdj(City* adj, int dim) {
-		if (Adj[dim] == nullptr || GetDist(Adj[dim], dim) > GetDist(adj, dim)) {
-			Adj[dim] = adj;
-		}
-	}
-	void Set(int idx, string& name, int x, int y) { Idx = idx; Name = name; Pos[0] = x; Pos[1] = y; }
-};
-
-vector<string> closestStraightCity(vector<string> cs, vector<int> xs, vector<int> ys, vector<string> qs) {
-	vector<City> cities;
-	map<string, City*> citiesMap;
-	map<int, vector<int>> distsX;
-	map<int, vector<int>> distsY;
-
-	cities.resize(cs.size());
-	for (int i = 0; i < cs.size(); ++i) {
-		string& name = cs[i]; int x = xs[i]; int y = ys[i];
-		cities[i].Set(i, name, x, y);
-		citiesMap[name] = &cities[i];
-		distsX[x].push_back(i);
-		distsY[y].push_back(i);
-	}
-
-	int dim = 0;
-	std::function<bool(int, int)> compare = [&](int a, int b) {
-		City& ca = cities[a]; City& cb = cities[b];
-		return ca.Pos[dim] < cb.Pos[dim];
-	};
-
-	map<int, vector<int>>* iterations[]{ &distsY, &distsX };
-	for (int i = 0; i < 2; ++i) {
-		map<int, vector<int>>::iterator iter = iterations[i]->begin();
-		for (; iter != iterations[i]->end(); ++iter) {
-			vector<int>& line = iter->second;
-			if (line.size() <= 1) continue;
-			dim = i;
-			std::sort(line.begin(), line.end(), compare);
-			for (int j = 0; j < line.size() - 1; ++j) {
-				City& curr = cities[line[j]]; City& next = cities[line[j + 1]];
-				curr.SetAdj(&next, dim); next.SetAdj(&curr, dim);
-			}
-		}
-	}
-
-	const string NONE = "NONE";
-	vector<string> outputs(qs.size());
-	map<int, vector<int>>::iterator iterX, iterY;
-	map<string, City*>::iterator iterCity;
-	for (int i = 0; i < qs.size(); ++i) {
-		string output = NONE;
-		string& q = qs[i];
-		iterCity = citiesMap.find(q);
-		if (iterCity != citiesMap.end()) {
-			City& c = *citiesMap[q];
-			City* adj = c.GetNearst();
-			if (adj != nullptr) { output = adj->Name; }
-		}
-
-		outputs[i] = output;
-	}
-	return outputs;
+	return false;
 }
+template<typename T> static bool AddSafe(vector<T>& v, const T t) {
+	if (v.size() <= 0) {
+		v.push_back(t); return true;
+	}
+	typedef typename vector<T>::iterator iter_v;
+	iter_v iter = std::find(v.begin(), v.end(), t);
+	if (v.end() == iter) {
+		v.push_back(t);
+		return true;
+	}
+	return false;
+}
+template<typename T> static bool Contains(vector<T>& v, const T t)
+{
+	typedef typename vector<T>::iterator iter_v;
+	iter_v iter = std::find(v.begin(), v.end(), t);
+	return iter != v.end();
+}
+template<typename T, typename V> static V Get(map<T, V>& m, const T t, V d)
+{
+	typedef typename map<T, V>::iterator iter_v;
+	iter_v iter = m.find(t);
+	return iter == m.end() ? d : iter->second;
+}
+int ToCom(int x, int y) { return x << 8 | y; }
+pair<int, int> ToXY(int com) {
+	pair<int, int> v(com >> 8, com & 0xff);
+	return v;
+}
+
+enum class EType { None = 0, Block, Gold, Goal };
+struct Node {
+	EType Type = EType::None;
+	int Idx = IDXNONE;
+	int X = -1, Y = -1;
+	vector<int> Adjs;
+	int ToXY() { return ToCom(X, Y); }
+	Node(int idx, EType type, int x, int y) { Idx = idx; Type = type; X = x; Y = y; }
+	void AddAdj(int adj) { AddSafe(Adjs, adj); }
+};
+void AddNode(vector<Node>& ns, map<int, int>& nsm, EType t, int x, int y) {
+	ns.push_back(Node(ns.size(), t, x, y));
+	Node* last = &ns[ns.size() - 1];
+	int com = last->ToXY();
+	nsm[com] = ns.size() - 1;
+}
+void AddAdj(vector<Node>& ns, map<int, int>& nsm, int x, int y) {
+	int k = ToCom(x, y);
+	Node& n = ns[Get(nsm, k, IDXNONE)];
+	vector<int> adjs(4);
+	adjs[0] = Get(nsm, ToCom(x + 1, y), IDXNONE);
+	adjs[1] = Get(nsm, ToCom(x, y + 1), IDXNONE);
+	adjs[2] = Get(nsm, ToCom(x - 1, y), IDXNONE);
+	adjs[3] = Get(nsm, ToCom(x, y - 1), IDXNONE);
+	for (int i = 0; i < 4; ++i) {
+		int adj = adjs[i];
+		if (adj == IDXNONE) continue;
+		n.AddAdj(adj);
+		ns[adj].AddAdj(n.Idx);
+	}
+}
+void BfsRec(vector<Node>& ns, vector<int> frs, map<int, bool>& visits, int& depth, bool skipGoal, function<bool(Node&)> call) {
+	vector<int> frsNext;
+	depth++;
+	for (int i = 0; i < frs.size(); ++i) {
+		int idx = frs[i];
+		visits[idx] = true;
+		Node& f = ns[idx];
+		bool isBreak = call(f);
+		if (isBreak == true) return;
+		for (int j = 0; j < f.Adjs.size(); ++j) {
+			int idxAdj = f.Adjs[j];
+			if (visits[idxAdj] == true) continue;
+			if (skipGoal == true && ns[idxAdj].Type == EType::Goal) continue;
+			AddSafe(frsNext, idxAdj);
+		}
+	}
+	if (frsNext.size() <= 0) return;
+	BfsRec(ns, frsNext, visits, depth, skipGoal, call);
+}
+
+int minMoves(vector<vector<int>> maze, int x, int y) {
+	vector<Node> ns;
+	map<int, int> nsm;
+	for (int i = 0; i < maze.size(); ++i) {
+		vector<int>& row = maze[i];
+		for (int j = 0; j < row.size(); ++j) {
+			EType type = (EType)row[j];
+			if (type == EType::Block) continue;
+			AddNode(ns, nsm, type, i, j);
+			AddAdj(ns, nsm, i, j);
+		}
+	}
+	ns[nsm[ToCom(x, y)]].Type = EType::Goal;
+
+	vector<int> golds, frs{ 0 };
+	map<int, bool> visits; bool foundGoal = false; int depth = 0;
+	BfsRec(ns, frs, visits, depth, false, [&](Node& n)
+		{
+			if (n.Type == EType::Gold) { AddSafe(golds, n.Idx); }
+			if (n.Type == EType::Goal) { foundGoal = true; }
+			return false;
+		});
+	if (false == foundGoal) {
+		return -1;
+	}
+
+	vector<int> gds = golds;
+	int pos = 0;
+	int move = 0;
+	// while(gds.size()>=1){
+	//     visits.clear(); frs.clear(); frs.push_back(pos); depth = 0;
+	//     BfsRec(ns, frs, visits, depth, true, [&](Node& n)
+	//     { 
+	//         if (n.Type==EType::Gold){ AddSafe(golds, n.Idx); } 
+
+	//     });
+	// }
+
+
+	int minmove = 0;
+	return minmove;
+}
+
 
 string ltrim(const string& str) {
 	string s(str);
@@ -109,83 +170,69 @@ string rtrim(const string& str) {
 	return s;
 }
 
+vector<string> split(const string& str) {
+	vector<string> tokens;
+
+	string::size_type start = 0;
+	string::size_type end = 0;
+
+	while ((end = str.find(" ", start)) != string::npos) {
+		tokens.push_back(str.substr(start, end - start));
+
+		start = end + 1;
+	}
+
+	tokens.push_back(str.substr(start));
+
+	return tokens;
+}
+
 int main()
 {
-	//ofstream fout(getenv("OUTPUT_PATH"));
+    //ofstream fout(getenv("OUTPUT_PATH"));
 
-	string c_count_temp;
-	getline(cin, c_count_temp);
+    string maze_rows_temp;
+    getline(cin, maze_rows_temp);
 
-	int c_count = stoi(ltrim(rtrim(c_count_temp)));
+    int maze_rows = stoi(ltrim(rtrim(maze_rows_temp)));
 
-	vector<string> c(c_count);
+    string maze_columns_temp;
+    getline(cin, maze_columns_temp);
 
-	for (int i = 0; i < c_count; i++) {
-		string c_item;
-		getline(cin, c_item);
+    int maze_columns = stoi(ltrim(rtrim(maze_columns_temp)));
 
-		c[i] = c_item;
-	}
+    vector<vector<int>> maze(maze_rows);
 
-	string x_count_temp;
-	getline(cin, x_count_temp);
+    for (int i = 0; i < maze_rows; i++) {
+        maze[i].resize(maze_columns);
 
-	int x_count = stoi(ltrim(rtrim(x_count_temp)));
+        string maze_row_temp_temp;
+        getline(cin, maze_row_temp_temp);
 
-	vector<int> x(x_count);
+        vector<string> maze_row_temp = split(rtrim(maze_row_temp_temp));
 
-	for (int i = 0; i < x_count; i++) {
-		string x_item_temp;
-		getline(cin, x_item_temp);
+        for (int j = 0; j < maze_columns; j++) {
+            int maze_row_item = stoi(maze_row_temp[j]);
 
-		int x_item = stoi(ltrim(rtrim(x_item_temp)));
+            maze[i][j] = maze_row_item;
+        }
+    }
 
-		x[i] = x_item;
-	}
+    string x_temp;
+    getline(cin, x_temp);
 
-	string y_count_temp;
-	getline(cin, y_count_temp);
+    int x = stoi(ltrim(rtrim(x_temp)));
 
-	int y_count = stoi(ltrim(rtrim(y_count_temp)));
+    string y_temp;
+    getline(cin, y_temp);
 
-	vector<int> y(y_count);
+    int y = stoi(ltrim(rtrim(y_temp)));
 
-	for (int i = 0; i < y_count; i++) {
-		string y_item_temp;
-		getline(cin, y_item_temp);
+    int result = minMoves(maze, x, y);
 
-		int y_item = stoi(ltrim(rtrim(y_item_temp)));
+    cout << result << "\n";
 
-		y[i] = y_item;
-	}
+    //cout.close();
 
-	string q_count_temp;
-	getline(cin, q_count_temp);
-
-	int q_count = stoi(ltrim(rtrim(q_count_temp)));
-
-	vector<string> q(q_count);
-
-	for (int i = 0; i < q_count; i++) {
-		string q_item;
-		getline(cin, q_item);
-
-		q[i] = q_item;
-	}
-
-	vector<string> result = closestStraightCity(c, x, y, q);
-
-	for (int i = 0; i < result.size(); i++) {
-		cout << result[i];
-
-		if (i != result.size() - 1) {
-			cout << "\n";
-		}
-	}
-
-	cout << "\n";
-
-	//cout.close();
-
-	return 0;
+    return 0;
 }
